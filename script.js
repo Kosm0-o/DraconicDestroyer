c49d1b62fb9b33898e9d1a7155f3136a316c54b9
Kosm0-o <logantreboutat@gmail.com>
2024-09-24 17:18:08 -0700

    fix current weapon last update

--- script.js
+++ script.js
@@ -306,7 +306,7 @@ function attack() {
   if (Math.random() <= .1 && inventory.length !== 1) {
     text.innerText += " Your " + inventory.pop() + " breaks.";
     currentWeapon--;
-    currentWeaponText.innerText = currentWeapon;
+    currentWeaponText.innerText = weapons[currentWeapon].name;
     weaponCost = Math.floor(weaponCost / 1.5);
   }
 }
